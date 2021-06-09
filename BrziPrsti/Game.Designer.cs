ae86dd1166dcd3be8ba976477b40d37b0d94c6e8
Dragan Bozhinoski <dragan.bozhinoski@students.finki.ukim.mk>
2021-06-10 01:35:38 +0200

    Add project files.

---
+++ BrziPrsti/Game.Designer.cs
@@ -0,0 +1,164 @@
+﻿
+namespace BrziPrsti
+{
+    partial class Game
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.lblGeneratedWords = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.lblGuessed = new System.Windows.Forms.Label();
+            this.startTimer = new System.Windows.Forms.Timer(this.components);
+            this.pbMinuteTimer = new System.Windows.Forms.ProgressBar();
+            this.timerMinute = new System.Windows.Forms.Timer(this.components);
+            this.lblCount = new System.Windows.Forms.Label();
+            this.wpmlbl = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblGeneratedWords
+            // 
+            this.lblGeneratedWords.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblGeneratedWords.Location = new System.Drawing.Point(13, 13);
+            this.lblGeneratedWords.Name = "lblGeneratedWords";
+            this.lblGeneratedWords.Size = new System.Drawing.Size(873, 188);
+            this.lblGeneratedWords.TabIndex = 0;
+            this.lblGeneratedWords.Text = "Click the button to generate words..";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(374, 530);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(158, 51);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "GENERATE";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(374, 463);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(158, 51);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "I\'m ready, start the timer!";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(18, 242);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(873, 22);
+            this.textBox1.TabIndex = 3;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // lblGuessed
+            // 
+            this.lblGuessed.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblGuessed.ForeColor = System.Drawing.Color.Lime;
+            this.lblGuessed.Location = new System.Drawing.Point(17, 267);
+            this.lblGuessed.Name = "lblGuessed";
+            this.lblGuessed.Size = new System.Drawing.Size(876, 106);
+            this.lblGuessed.TabIndex = 4;
+            // 
+            // startTimer
+            // 
+            this.startTimer.Interval = 1000;
+            this.startTimer.Tick += new System.EventHandler(this.startTimer_Tick);
+            // 
+            // pbMinuteTimer
+            // 
+            this.pbMinuteTimer.Location = new System.Drawing.Point(18, 386);
+            this.pbMinuteTimer.Maximum = 60;
+            this.pbMinuteTimer.Name = "pbMinuteTimer";
+            this.pbMinuteTimer.Size = new System.Drawing.Size(873, 23);
+            this.pbMinuteTimer.TabIndex = 5;
+            this.pbMinuteTimer.Value = 60;
+            // 
+            // timerMinute
+            // 
+            this.timerMinute.Interval = 1000;
+            this.timerMinute.Tick += new System.EventHandler(this.timerMinute_Tick);
+            // 
+            // lblCount
+            // 
+            this.lblCount.AutoSize = true;
+            this.lblCount.Font = new System.Drawing.Font("Microsoft Sans Serif", 35F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCount.Location = new System.Drawing.Point(417, 282);
+            this.lblCount.Name = "lblCount";
+            this.lblCount.Size = new System.Drawing.Size(62, 67);
+            this.lblCount.TabIndex = 6;
+            this.lblCount.Text = "3";
+            this.lblCount.Visible = false;
+            // 
+            // wpmlbl
+            // 
+            this.wpmlbl.AutoSize = true;
+            this.wpmlbl.Location = new System.Drawing.Point(22, 483);
+            this.wpmlbl.Name = "wpmlbl";
+            this.wpmlbl.Size = new System.Drawing.Size(78, 17);
+            this.wpmlbl.TabIndex = 7;
+            this.wpmlbl.Text = "Your wpm: ";
+            // 
+            // Game
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(898, 593);
+            this.Controls.Add(this.wpmlbl);
+            this.Controls.Add(this.lblCount);
+            this.Controls.Add(this.pbMinuteTimer);
+            this.Controls.Add(this.lblGuessed);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.lblGeneratedWords);
+            this.Name = "Game";
+            this.Text = "Game";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblGeneratedWords;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label lblGuessed;
+        private System.Windows.Forms.Timer startTimer;
+        private System.Windows.Forms.ProgressBar pbMinuteTimer;
+        private System.Windows.Forms.Timer timerMinute;
+        private System.Windows.Forms.Label lblCount;
+        private System.Windows.Forms.Label wpmlbl;
+    }
+}
---
+++ BrziPrsti/Game.cs
@@ -0,0 +1,197 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BrziPrsti
+{
+    public partial class Game : Form
+    {
+        private List<string> rechenice;
+        private int guessedWords;
+        private string textZaPogoduvanje;
+        private int start;
+        private int seconds;
+        private float wpm;
+        public Game()
+        {
+            InitializeComponent();
+            rechenice = new List<string>() { 
+            "Can you feel the sunshine? Does it brighten up your day? Don't you feel that sometimes you just need to run away? Reach out for the sunshine, forget about the rain. Just think about the good times and they will come back again.",
+            "You gotta see it to believe it, the sky never looked so blue it's so hard to leave it, but that's what I always do. So I keep thinking back to a time under the canyon moon.",
+            "with as then they how they form become same good no high which again find govern run home present govern these do can want become off",
+            "Close your eyes in the night and I'll be right in front of you. So sit back, enjoy the view until the morning. When the sun comes from the west, that's where you'll find my silhouette. I'll ride a cloud, come back to you without a warning. So don't speak, 'cause it only makes my heart bleed. No matter what you say, I'll be only just a call away.",
+            "We're no strangers to love. You know the rules and so do I. A full commitment's what I'm thinkin' of. You wouldn't get this from any other guy.",
+            "You don't need money, don't take fame. Don't need no credit card to ride this train. It's strong and it's sudden and it's cruel sometimes, but it might just save your life. That's the power of love.",
+            "All I'm asking is that you do the minimal amount of work in this class to give yourself the illusion that you're actually learning something, and to give me a modicum of self respect like I'm actually teaching a class.",
+            "In the late 1960s, a growing number of people became concerned about the growing problems of pollution and the destruction of natural habitats. This movement led to the formation of groups like the Environmental Protection Agency (EPA). The primary goal of the environmental movement as a whole is to make sure that the environment is safe and intact for future generations to enjoy.",
+            "I want to be very small, and I want you to put me in your pocket, and I wanna stay there and you can just reach in whenever you want and just pat me on the head.",
+            "Never imagine yourself not to be otherwise than what it might appear to others that what you were or might have been was not otherwise than what you had been would have appeared to them to be otherwise.",
+            "You never should settle for the lifetime that is handed to you. There's always a line to be cut and someone to barrel through. And if you should find that you're about to get the short of the stick, take what you want, return what you get.",
+            "I know just how to whisper and I know just how to cry. I know just where to find the answers and I know just how to lie. I know just how to fake it and I know just how to scheme. I know just when to face the truth and then I know just when to dream.",
+            "If someone is able to show me that what I think or do is not right, I will happily change, for I seek the truth, by which no one was ever truly harmed. It is the person who continues in his self-deception and ignorance who is harmed.",
+            "Opening your eyes is all that is needing. The heart lies and the head plays tricks with us, but the eyes see true. Look with your eyes. Hear with your ears. Taste with your mouth. Smell with your nose. Feel with your skin. Then comes the thinking, afterward, and in that way knowing the truth.",
+            "The sooner your kids appreciate the value of work, the more successful they will be. Work is part of life. You work to earn money, put food on the table, and keep your homes orderly and clean. For your kids, work involves schoolwork, homework, and teamwork at home and in the community."
+            };
+            textZaPogoduvanje = "Hello, Hope you will check here :)";
+            reset();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            generate();
+        }
+        private void reset()
+        { 
+            //boxes
+            lblGuessed.Text = "";
+            lblGeneratedWords.Text = "Click the button to generate words!";
+            textBox1.Text = "Click the button to generate words!";
+            // VALUES
+            guessedWords = 0;
+            start = 3;
+            seconds = 60;
+            pbMinuteTimer.Value = 60;
+            wpm = 0;
+            //TIMERS
+            timerMinute.Stop();
+            startTimer.Stop();
+            //TB-buttons
+            textBox1.ReadOnly = true;
+            button1.Enabled = true;
+            button2.Enabled = true;
+        }
+        private void generate()
+        {
+            reset();
+            Random rand = new Random();
+            int a = rand.Next(0, rechenice.Count);
+            textZaPogoduvanje = rechenice[a];
+            lblGeneratedWords.Text = textZaPogoduvanje;
+            textBox1.Text = "Type the above text here when the race begins!";
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            if (textBox1.Text != "Type the above text here when the race begins!" && textBox1.Text != "Click the button to generate words!")
+            {
+                if (guessedWords == textZaPogoduvanje.Split().Length)
+                {
+                    reset();
+                    if(wpm < 80)
+                    {
+                        if (MessageBox.Show("Congrats, you finished early.\nWPM: " + wpm.ToString() + "\nNEW GAME?", "GOOD JOB!", MessageBoxButtons.YesNo) == DialogResult.Yes)
+                        {
+                            generate();
+                        }
+                        else
+                        {
+                            textBox1.ReadOnly = true;
+                        }
+                    }
+                    if (wpm > 100)
+                    {
+                        if (MessageBox.Show("YOU ARE OCTOPUS! GOOD JOB!\nWPM: " + wpm.ToString() + "\nNEW GAME?", "GOOD JOB!", MessageBoxButtons.YesNo) == DialogResult.Yes)
+                        {
+                            generate();
+                        }
+                        else
+                        {
+                            textBox1.ReadOnly = true;
+                        }
+                    }
+                    else
+                    {
+                        if (MessageBox.Show("Congrats, you are middle-range writer!\nWPM: " + wpm.ToString() + "\nNEW GAME?", "GOOD JOB!", MessageBoxButtons.YesNo) == DialogResult.Yes)
+                        {
+                            generate();
+                        }
+                        else
+                        {
+                            textBox1.ReadOnly = true;
+                        }
+                    }
+
+                }
+                else
+                {
+                    string proverka = textZaPogoduvanje.Split()[guessedWords] + " ";
+                    if (textBox1.Text == proverka)
+                    {
+                        guessedWords += 1;
+                        lblGuessed.Text += textBox1.Text + " ";
+                        wpm = guessedWords / ((float)seconds / 60);
+                        wpmlbl.Text = wpm.ToString();
+                        textBox1.Text = "";
+                    }
+                    else
+                    {
+                        if (!proverka.Contains(textBox1.Text))
+                        {
+                            lblGeneratedWords.ForeColor = Color.Red;
+                        }
+                        else
+                        {
+                            lblGeneratedWords.ForeColor = Color.Black;
+                        }
+                    }
+                }
+            }
+        }
+
+        private void startTimer_Tick(object sender, EventArgs e)
+        {
+            if(start > 0)
+            {
+                start -= 1;
+                lblCount.Text = start.ToString();
+                if (start == 0)
+                    lblCount.Text = "GO!";
+            }
+            else if(start==0)
+            {
+                lblCount.Text = "GO!";
+                lblCount.Visible = false;
+                textBox1.Text = "";
+                textBox1.ReadOnly = false;
+                timerMinute.Start();
+                start = -20;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            generate();
+            startTimer.Start();
+            button1.Enabled = false;
+            button2.Enabled = false;
+            lblCount.Visible = true;
+        }
+
+        private void timerMinute_Tick(object sender, EventArgs e)
+        {
+            if (seconds > 0)
+            {
+                seconds -= 1;
+                pbMinuteTimer.Value = seconds;
+            }
+            else if (seconds == 0)
+            {
+                seconds = -10;
+                if (MessageBox.Show("You did not finish your game in time. Give me another try?", "I'm sorry!", MessageBoxButtons.YesNo) == DialogResult.Yes)
+                {
+                    generate();
+                }
+                else
+                {
+                    reset();
+                }
+            }
+        }
+    }
+}
